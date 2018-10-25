3cb9d7e78d59e491d28a21dcda5a62b66439ea08
maks3217 <maks3217@wp.pl>
2018-10-25 19:30:14 +0200

    The main commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace Kohonen
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Point> punkty = new List<Point>();
+        private void Pole_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point punkt;
+            punkt = Mouse.GetPosition(sender as IInputElement);
+            Ellipse myEllipse = CreateEllipse(6, 6, punkt.X, punkt.Y, Color.FromRgb(0, 0, 0));
+            Pole.Children.Add(myEllipse);
+            punkty.Add(punkt);
+        }
+
+
+        Ellipse CreateEllipse(double width, double height, double desiredCenterX, double desiredCenterY, Color kolor)
+        {
+            Ellipse ellipse = new Ellipse { Width = width, Height = height };
+            double left = desiredCenterX - (width / 2);
+            double top = desiredCenterY - (height / 2);
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            mySolidColorBrush.Color = kolor;
+            ellipse.Fill = mySolidColorBrush;
+            ellipse.Margin = new Thickness(left, top, 0, 0);
+            return ellipse;
+        }
+
+        Line CreateLine(Point punkt1, Point punkt2)
+        {
+            Line myLine = new Line();
+            myLine.Stroke = System.Windows.Media.Brushes.Black;
+            myLine.X1 = punkt1.X;
+            myLine.X2 = punkt2.X;
+            myLine.Y1 = punkt1.Y;
+            myLine.Y2 = punkt2.Y;
+            myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            myLine.VerticalAlignment = VerticalAlignment.Center;
+            myLine.StrokeThickness = 2;
+            return myLine;
+        }
+
+        private void Kohonen_Click(object sender, RoutedEventArgs e)
+        {
+            Siec siec = new Siec(punkty.Count);
+            siec.Learn(100000, punkty);
+            Rysuj_Siec(siec.Neurony);
+        }
+
+        void Rysuj_Siec(List<Point> punkty)
+        {
+            Ellipse elipse;
+            Line line;
+            for (int i = 0; i < punkty.Count; i++)
+            {
+                elipse = CreateEllipse(10, 10, punkty[i].X, punkty[i].Y, Color.FromRgb(200, 0, 0));
+                Pole.Children.Add(elipse);
+            }
+            for (int i = 1; i < punkty.Count; i++)
+            {
+                line = CreateLine(punkty[i - 1], punkty[i]);
+                Pole.Children.Add(line);
+            }
+
+            line = CreateLine(punkty[punkty.Count-1], punkty[0]);
+            Pole.Children.Add(line);
+        }
+    }
+}
---
+++ Siec.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kohonen
+{
+    class Siec
+    {
+        Random random = new Random();
+        int Ile_neuronow;
+        public List<Point> Neurony = new List<Point>();
+        public Siec(int Ile_neuronow)
+        {
+            this.Ile_neuronow = Ile_neuronow;
+            for (int i = 0; i < Ile_neuronow; i++)
+            {
+                double X = random.NextDouble() * 600;
+                double Y = random.NextDouble() * 600;
+                Point neuron = new Point(X, Y);
+                Neurony.Add(neuron);
+            }
+        }
+
+        public void Learn(int T, List<Point> punkty)
+        {
+            for (int t = 0; t < T; t++)
+            {
+                int indeks = random.Next() % punkty.Count;
+                int winner = Winner(punkty[indeks]);
+                ZmienWagi(winner, punkty[indeks], t, T);
+
+            }
+            Debug.WriteLine("siec nauczona");
+        }
+
+        void ZmienWagi(int i, Point punkt, double t, double T)
+        {
+            for (int j = 0; j < Ile_neuronow; j++)
+            {
+                Point neuron = Neurony[j];
+                neuron.X = Neurony[j].X + ((T - t) / T) * G(i, j) * (punkt.X - neuron.X);
+                neuron.Y = Neurony[j].Y + ((T - t) / T) * G(i, j) * (punkt.Y - neuron.Y);
+                Neurony[j] = neuron;
+            }
+        }
+
+        double G(int i, int j)
+        {
+            double lamda = 1;
+            return Math.Exp((-1 * Ro(i,j) * Ro(i,j)) / (2 * lamda * lamda));
+        }
+
+        int Ro(int i, int j)
+        {
+            return Math.Min(Math.Abs(i - j), Math.Abs(i - (j + Ile_neuronow)));
+        }
+
+        int Winner(Point punkt)
+        {
+            int winer = 0;
+            double min_lenght = lenght(Neurony[0], punkt);
+            for (int i = 1; i < Ile_neuronow; i++)
+            {
+                if (lenght(Neurony[i], punkt) < min_lenght)
+                {
+                    min_lenght = lenght(Neurony[i], punkt);
+                    winer = i;
+                }
+            }
+            return winer;
+        }
+
+        double lenght(Point punkt1, Point punkt2)
+        {
+            return Math.Sqrt(((punkt1.X - punkt2.X) * (punkt1.X - punkt2.X)) + ((punkt1.Y - punkt2.Y) * (punkt1.Y - punkt2.Y)));
+        }
+
+
+    }
+}
